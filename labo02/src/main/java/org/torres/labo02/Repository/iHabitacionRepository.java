cc11579bc4aa4f92411dd651521a234a70f6b67e
Fabio Mijango <00156022@uca.edu.sv>
2025-05-07 19:50:45 -0600

    ADD: Implement iHabitacionRepository and HabitacionService for managing Habitacion entities

---
+++ labo02/src/main/java/org/torres/labo02/Repository/iHabitacionRepository.java
@@ -0,0 +1,22 @@
+package org.torres.labo02.Repository;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.torres.labo02.Domain.Entities.Habitacion;
+
+import java.util.List;
+import java.util.UUID;
+
+@Transactional
+public interface iHabitacionRepository extends iGenericRepository<Habitacion, UUID>{
+
+    @Query(value = "SELECT h FROM Habitacion h WHERE h.piso = :pisoId")
+    public List<Habitacion> findHabitacionByPiso(@Param("pisoId") UUID pisoId);
+
+
+    @Query(nativeQuery = true,
+    value = "SELECT * FROM Habitacion WHERE Habitacion.nombreHabitacion = :nombreHabitacion")
+    public Habitacion findHabitacionByHabitacion_id(@Param("habitacion_id") UUID habitacion_id);
+
+}
