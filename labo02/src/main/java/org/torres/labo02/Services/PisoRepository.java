d54ae4d55e1b115a3c81a285cd549eb416735f4b
Fabio Mijango <00156022@uca.edu.sv>
2025-05-07 19:23:37 -0600

    ADD: Implement iPisoRepository and PisoRepository for managing Piso entities

---
+++ labo02/src/main/java/org/torres/labo02/Services/PisoRepository.java
@@ -0,0 +1,30 @@
+package org.torres.labo02.Services;
+
+import org.torres.labo02.Domain.Entities.Piso;
+import org.torres.labo02.Repository.iPisoRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class PisoRepository {
+
+    private iPisoRepository pisoRepository;
+
+    public void createPiso(Piso piso) {
+        pisoRepository.save(piso);
+    }
+
+    public List<Piso> getAllPisos() {
+        return pisoRepository.findAll();
+    }
+
+    public Piso getPisoBySucursalId(UUID sucursalId) {
+        return pisoRepository.getPisoBySucursalId(sucursalId);
+    }
+
+    public Piso getPisoByNombrePiso(String nombrePiso) {
+        return pisoRepository.getPisoByNombrePiso(nombrePiso);
+    }
+
+
+}
